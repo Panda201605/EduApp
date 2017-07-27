b82064f6ee9ddf2eb00ba52cb0e0b5045a938aa9
pansp <547521091@qq.com>
2017-07-27 20:26:40 +0800

    build

--- app/src/main/java/com/linchuanedu/edu/eduapp/view/MainActivity.java
+++ app/src/main/java/com/linchuanedu/edu/eduapp/view/MainActivity.java
@@ -1,14 +1,39 @@
 package com.linchuanedu.edu.eduapp.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import com.linchuanedu.edu.eduapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+    private Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.button1);
+        btn2 = (Button) findViewById(R.id.button2);
+
+        Log.d("test","------test------");
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/linchuanedu/edu/eduapp/view/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.linchuanedu.edu.eduapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.linchuanedu.edu.eduapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
